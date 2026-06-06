ed5ea9abdeb2ee33123bdd5aefd88c02ee348ae3
pratheek-rai14 <pratheekrai14@gmail.com>
2026-06-07 01:05:48 +0530

    Updated feature

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add token authomatically to every request 
